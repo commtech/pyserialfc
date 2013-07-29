bfbf3585b2a501068ba0b957157296946a4d1b14
William Fagan <willf@commtech-fastcom.com>
2013-07-29 10:57:44 -0500

    Bump version for release.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pyserialfc',
-      version='1.0.0',
+      version='1.1.0',
       py_modules=['serialfc'],
       requires=['serial', 'win32'],
       )
